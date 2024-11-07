f44bd1867cca533d39c2bef44d2e80e78111a9a3
Elliot Robinson <elliot.robinson92@hotmail.com>
2024-11-07 20:25:15 +0000

    convert to custom hook to allow use of useNavigate

--- src/lib/login.ts
+++ src/lib/login.ts
@@ -1,21 +1,28 @@
 import { supabase } from "./supabaseClient";
+import { useNavigate } from "react-router-dom";
 
-export const login = async (email: string, password: string) => {
-  try {
-    const { data, error: dbError } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    });
+export const useLogin = () => {
+  const navigate = useNavigate();
 
-    if (dbError) {
-      throw dbError;
+  const login = async (email: string, password: string) => {
+    try {
+      const { data, error: dbError } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
+
+      if (dbError) {
+        throw dbError;
+      }
+
+      if (data) {
+        console.log("LOGIN SUCCESSFUL:", data);
+        navigate("/dashboard");
+      }
+    } catch (error: any) {
+      throw new Error(error.message);
     }
+  };
 
-    if (data) {
-      console.log("LOGIN SUCCESSFUL:", data);
-      return data;
-    }
-  } catch (error: any) {
-    throw new Error(error.message);
-  }
+  return login;
 };
