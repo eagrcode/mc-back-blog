6c3b4a99220e951cb03068f6d1f4dbd82c284613
Elliot Robinson <elliot.robinson92@hotmail.com>
2024-11-07 20:39:57 +0000

    add fetch user details upon navigation, redirect if no user

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+import { getUser } from "./lib/getUser";
+import { useNavigate } from "react-router-dom";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const [user, setUser] = useState<any>(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const loggedInUser = await getUser();
+
+      if (!loggedInUser) {
+        navigate("/");
+      }
+
+      setUser(loggedInUser);
+    };
+    fetchUser();
+  }, [navigate]);
+
+  return <div>{`Hello, ${user?.email}`}</div>;
 }
